cca49964d3c407f133746b9b9a2e7b029ba3ca59
aalfutbol <kkemallaziz@gmail.com>
2025-01-18 01:27:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -26,37 +26,37 @@ app.post('/submit', (req, res) => {
 });
 
 async function createMatchImage(data) {
-  const canvas = createCanvas(800, 600); // Canvas boyutlarını ayarlıyoruz
+  const canvas = createCanvas(2537, 1800); // Yeni boyutları ayarlıyoruz
   const ctx = canvas.getContext('2d');
 
-  // Arka plan rengi
-  ctx.fillStyle = '#f0f0f0';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // Arka planı ekliyoruz
+  const background = await loadImage('/Users/kemalaziz/newpro/public/logos/macsonucu.png'); // Arka plan görselini yüklüyoruz
+  ctx.drawImage(background, 0, 0, canvas.width, canvas.height); // Arka planı canvas'a ekliyoruz
 
   // Takım logolarını ekleyelim
   const homeTeamLogo = await loadImage(path.join(__dirname, 'public', 'logos', `${data.homeTeam}.png`));
   const awayTeamLogo = await loadImage(path.join(__dirname, 'public', 'logos', `${data.awayTeam}.png`));
   
   // Ev sahibi takım logosunu ekleme
-  ctx.drawImage(homeTeamLogo, 50, 50, 100, 100);
+  ctx.drawImage(homeTeamLogo, 100, 150, 200, 200); // Boyut ve pozisyonu ayarlıyoruz
 
   // Deplasman takım logosunu ekleme
-  ctx.drawImage(awayTeamLogo, 650, 50, 100, 100);
+  ctx.drawImage(awayTeamLogo, 2237, 150, 200, 200); // Boyut ve pozisyonu ayarlıyoruz
 
   // Takım isimlerini yazma
-  ctx.font = '24px Arial';
+  ctx.font = '48px Arial';
   ctx.fillStyle = '#000';
-  ctx.fillText(data.homeTeam, 50, 170); // Ev sahibi takım ismi
-  ctx.fillText(data.awayTeam, 650, 170); // Deplasman takım ismi
+  ctx.fillText(data.homeTeam, 100, 400); // Ev sahibi takım ismi
+  ctx.fillText(data.awayTeam, 2237, 400); // Deplasman takım ismi
 
   // Gol atan oyuncuları yazma
-  ctx.font = '20px Arial';
-  ctx.fillText(`Gol atan oyuncular: ${data.homeGoals}`, 50, 200); // Ev sahibi oyuncular
-  ctx.fillText(`Gol atan oyuncular: ${data.awayGoals}`, 650, 200); // Deplasman oyuncular
+  ctx.font = '36px Arial';
+  ctx.fillText(`Gol atan oyuncular: ${data.homeGoals}`, 100, 480); // Ev sahibi oyuncular
+  ctx.fillText(`Gol atan oyuncular: ${data.awayGoals}`, 2237, 480); // Deplasman oyuncular
 
   // Sonuç: Ev sahibi - Deplasman
-  ctx.font = '30px Arial';
-  ctx.fillText(`${data.homeScore} - ${data.awayScore}`, 375, 300); // Skor
+  ctx.font = '72px Arial';
+  ctx.fillText(`${data.homeScore} - ${data.awayScore}`, 1290, 900); // Skor
 
   // Görseli kaydediyoruz
   const buffer = canvas.toBuffer('image/png');
